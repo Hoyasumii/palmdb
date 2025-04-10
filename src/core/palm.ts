d036442efee18229666233f1347ad351094434db
Hoyasumii <alanreisanjo@gmail.com>
2025-04-10 20:00:49 -0300

    Rasifude

--- src/core/palm.ts
+++ src/core/palm.ts
@@ -1,25 +1,25 @@
-import type { PalmConfig } from "@/types";
 import type { CollectionInterface } from "@/types/collection-interface";
 import type { PalmInterface } from "@/types/palm-interface";
-import type { z } from "zod";
-import config from "~/palm.config";
-import type { Coconut } from "./coconut";
+import palmConfig from "~/palm.config";
+import { Coconut } from "./coconut";
+import type { ZodObject, ZodRawShape } from "zod";
 
-export class Palm<
-  T extends keyof typeof config.schema,
-  S extends Record<string, z.ZodObject<any>> = typeof config.schema
-> implements PalmInterface<T, z.infer<S[T]>>
-{
-  constructor(
-    public config: PalmConfig<T, z.TypeOf<S[T]>>,
-    private coconut: Coconut
-  ) {}
+type PalmSchemaKeys = keyof typeof palmConfig.schema;
+type PalmSchemaValues = Record<PalmSchemaKeys, ZodObject<ZodRawShape>>;
+
+export class Palm implements PalmInterface<PalmSchemaKeys, PalmSchemaValues> {
+  config = palmConfig;
+  coconut = new Coconut();
 
   database: { export: null; import: null } = { export: null, import: null };
   init = null;
   migrate = null;
 
-  select(target: T): CollectionInterface<z.TypeOf<S[T]>[T]> {
+  select(
+    target: PalmSchemaKeys
+  ): CollectionInterface<PalmSchemaValues[PalmSchemaKeys]> {
     throw new Error("Method not implemented.");
   }
+
+  fs = null;
 }
